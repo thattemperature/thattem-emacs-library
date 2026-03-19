35eab7e2cee900cff78914fff255024a81a15bc1
thattemperature <2719023332@qq.com>
2026-03-19 14:27:17 +0800

    Final: add network speed monitor.

--- src/thattem_emacs_library.c
+++ src/thattem_emacs_library.c
@@ -44,12 +44,33 @@ static void register_swap_usage(emacs_env *env) {
   env->funcall(env, env->intern(env, "defalias"), 2, args);
 }
 
+static emacs_value net_speed(emacs_env *env, ptrdiff_t nargs,
+                             emacs_value args[], void *data) {
+  double in, out;
+  get_net_speed(&in, &out);
+  emacs_value in_value = env->make_float(env, in);
+  emacs_value out_value = env->make_float(env, out);
+  emacs_value values[] = {in_value, out_value};
+  return env->funcall(env, env->intern(env, "list"), 2, values);
+}
+static void register_net_speed(emacs_env *env) {
+  init_net_speed();
+
+  emacs_value func = env->make_function(
+      env, 0, 0, net_speed,
+      "Return network speed as a list of floating point number.", NULL);
+  emacs_value symbol = env->intern(env, "thattem-net-speed");
+  emacs_value args[] = {symbol, func};
+  env->funcall(env, env->intern(env, "defalias"), 2, args);
+}
+
 int emacs_module_init(struct emacs_runtime *runtime) {
   emacs_env *env = runtime->get_environment(runtime);
 
   register_cpu_usage(env);
   register_mem_usage(env);
   register_swap_usage(env);
+  register_net_speed(env);
 
   return 0;
 }
