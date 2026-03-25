2ab13cddbbd4870ecc5b0c9ba430e09397976806
thattemperature <2719023332@qq.com>
2026-03-25 23:04:16 +0800

    Detail the emacs function document string.

--- src/thattem_emacs_library.c
+++ src/thattem_emacs_library.c
@@ -10,9 +10,14 @@ static emacs_value cpu_usage(emacs_env *env, ptrdiff_t nargs,
 static void register_cpu_usage(emacs_env *env) {
   init_cpu_usage();
 
-  emacs_value func =
-      env->make_function(env, 0, 0, cpu_usage,
-                         "Return CPU usage as a floating point number.", NULL);
+  emacs_value func = env->make_function(
+      env, 0, 0, cpu_usage,
+      "Return CPU usage as a floating point number.\n"
+      "\n"
+      "The usage is calculated by this formula:\n"
+      " (user time + system time + nice time) / total_time\n"
+      "Start timing from the previous call of this function.",
+      NULL);
   emacs_value symbol = env->intern(env, "thattem-cpu-usage");
   emacs_value args[] = {symbol, func};
   env->funcall(env, env->intern(env, "defalias"), 2, args);
@@ -23,9 +28,13 @@ static emacs_value mem_usage(emacs_env *env, ptrdiff_t nargs,
   return env->make_float(env, get_mem_usage());
 }
 static void register_mem_usage(emacs_env *env) {
-  emacs_value func = env->make_function(
-      env, 0, 0, mem_usage, "Return memory usage as a floating point number.",
-      NULL);
+  emacs_value func =
+      env->make_function(env, 0, 0, mem_usage,
+                         "Return memory usage as a floating point number.\n"
+                         "\n"
+                         "The usage is calculated by this formula:\n"
+                         "used memory / total memory",
+                         NULL);
   emacs_value symbol = env->intern(env, "thattem-mem-usage");
   emacs_value args[] = {symbol, func};
   env->funcall(env, env->intern(env, "defalias"), 2, args);
@@ -38,7 +47,11 @@ static emacs_value swap_usage(emacs_env *env, ptrdiff_t nargs,
 static void register_swap_usage(emacs_env *env) {
   emacs_value func =
       env->make_function(env, 0, 0, swap_usage,
-                         "Return swap usage as a floating point number.", NULL);
+                         "Return swap usage as a floating point number.\n"
+                         "\n"
+                         "The usage is calculated by this formula:\n"
+                         "used swap / total swap",
+                         NULL);
   emacs_value symbol = env->intern(env, "thattem-swap-usage");
   emacs_value args[] = {symbol, func};
   env->funcall(env, env->intern(env, "defalias"), 2, args);
@@ -58,7 +71,13 @@ static void register_net_speed(emacs_env *env) {
 
   emacs_value func = env->make_function(
       env, 0, 0, net_speed,
-      "Return network speed as a list of floating point number.", NULL);
+      "Return network speed as a list of floating point number.\n"
+      "\n"
+      "The first element is the download speed and the second one is upload.\n"
+      "It is calculated by this formula:\n"
+      "(current traffic - previous traffic) / interval time\n"
+      "Start timing from the previous call of this function.",
+      NULL);
   emacs_value symbol = env->intern(env, "thattem-net-speed");
   emacs_value args[] = {symbol, func};
   env->funcall(env, env->intern(env, "defalias"), 2, args);
