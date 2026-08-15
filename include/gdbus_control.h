f2087dcec84b30a53b7740177f418e54dce79762
thattemperature <2719023332@qq.com>
2026-08-15 16:32:46 +0800

    Add include guards.

--- include/gdbus_control.h
+++ include/gdbus_control.h
@@ -1,3 +1,6 @@
+#ifndef GDBUS_CONTROL_H
+#define GDBUS_CONTROL_H
+
 #include <glib.h>
 
 int switch_workspace_to(int index, GError **error);
@@ -5,3 +8,5 @@ int switch_workspace_to(int index, GError **error);
 int get_active_workspace(GError **error);
 
 int get_workspace_count(GError **error);
+
+#endif
--- include/gtop_data.h
+++ include/gtop_data.h
@@ -1,3 +1,6 @@
+#ifndef GTOP_DATA_H
+#define GTOP_DATA_H
+
 void init_cpu_usage(void);
 double get_cpu_usage(void);
 
@@ -7,3 +10,5 @@ double get_swap_usage(void);
 
 void init_net_speed(void);
 void get_net_speed(double *in_speed, double *out_speed);
+
+#endif
